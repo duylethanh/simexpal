1cd0ad15e1825fe78adaacdaf7cd4f55e29009c6
Duy Le Thanh <lethanhd@informatik.hu-berlin.de>
2021-01-23 17:45:51 +0100

    util, tests/validation: Cache validation results, fixes #94

--- tests/validation/test_experiments_yml.py
+++ tests/validation/test_experiments_yml.py
@@ -15,6 +15,7 @@ invalid_experiments_ymls = ['invalid_ymls/top_level-additional_property.yml',
 @pytest.mark.parametrize('rel_yml_path', valid_experiments_ymls)
 def test_valid_experiments_yml(rel_yml_path):
     yml_dir = os.path.join(file_dir, rel_yml_path)
+    util.try_rmfile(os.path.join(yml_dir, 'validation.cache'))  # Make sure that caches from previous calls are deleted.
     ret_val = util.validate_setup_file(yml_dir, 'experiments.yml', 'experiments.json')
 
     assert isinstance(ret_val, dict)
