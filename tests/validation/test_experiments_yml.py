1848acdd9990a3658fe3062b4b4415372b7b3530
Duy Le Thanh <lethanhd@informatik.hu-berlin.de>
2021-01-23 16:09:19 +0100

    base, util, tests/validation: Refactor 'base.config_for_dir()'
    In preparation of caching validation results.

--- tests/validation/test_experiments_yml.py
+++ tests/validation/test_experiments_yml.py
@@ -5,23 +5,27 @@ import pytest
 from simexpal import util
 
 file_dir = os.path.abspath(os.path.dirname(__file__))
-valid_experiments_ymls = ['/../../examples/sorting/experiments.yml',
-                          '/../../examples/sorting_cpp/experiments.yml',
-                          '/../../examples/download_instances/experiments.yml']
+valid_experiments_ymls = ['../../examples/sorting/',
+                          '../../examples/sorting_cpp/',
+                          '../../examples/download_instances/']
 
-invalid_experiments_ymls = ['/invalid_ymls/top_level-additional_property.yml',
-                            '/invalid_ymls/repo-invalid_value.yml']
+invalid_experiments_ymls = ['invalid_ymls/top_level-additional_property.yml',
+                            'invalid_ymls/repo-invalid_value.yml']
 
 @pytest.mark.parametrize('rel_yml_path', valid_experiments_ymls)
 def test_valid_experiments_yml(rel_yml_path):
-    ret_val = util.validate_setup_file(file_dir + rel_yml_path)
+    yml_dir = os.path.join(file_dir, rel_yml_path)
+    ret_val = util.validate_setup_file(yml_dir, 'experiments.yml', 'experiments.json')
 
     assert isinstance(ret_val, dict)
-    assert 'instdir' in ret_val
 
 @pytest.mark.parametrize('rel_yml_path', invalid_experiments_ymls)
 def test_invalid_experiments_yml(rel_yml_path):
+    rel_yml_dir = os.path.dirname(rel_yml_path)
+    yml_dir = os.path.join(file_dir, rel_yml_dir)
+    filename = os.path.basename(rel_yml_path)
+
     with pytest.raises(SystemExit) as info:
-        util.validate_setup_file(file_dir + rel_yml_path)
+        util.validate_setup_file(yml_dir, filename, 'experiments.json')
 
     assert info.value.code == 1
